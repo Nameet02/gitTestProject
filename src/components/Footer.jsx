8287f4199f56756a38ec1bfd725ef3c25d5d5e9e
rachanasen346 <rachanasen1999@gmail.com>
2024-09-12 18:20:47 +0530

    Added Navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black mb-0'>
+      <ul>
+        <li>List 1</li>
+        <li>List 1</li>
+        <li>List 1</li>
+        <li>List 1</li>
+        <li>List 1</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
