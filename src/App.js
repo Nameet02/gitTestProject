460d7ae6afda3917c8b7db58c5a5ef3c5f37c14d
Nameet02 <nameetm709@gmail.com>
2024-09-13 17:31:09 +0530

    initial Project setup

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProfileTab from "./components/ProfileTab";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Protected from "./route/Protected";
+import ShopPage from "./pages/ShopPage";
+import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/profile"
-            element={
-              <Protected>
-                <ProfileTab />
-              </Protected>
-            }
-          />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/shop" element={<ShopPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
